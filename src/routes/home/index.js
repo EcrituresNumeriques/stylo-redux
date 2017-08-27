baad7a4c5dfe4105864d7261dfeb399db758bf68
PookMook <arthur.juchereau@gmail.com>
2017-08-26 20:07:13 -0400

    editable nextVersion

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,44 +6,46 @@ export default class Home extends Component {
 	constructor(props) {
 		super(props);
     this.plotDiff = this.plotDiff.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+		this.state = {firstVersion:'Contrary to belief, Lorem Ipsum is not just a random text. It has roots in a piece of classical Latin literature from 75 BC, making it over 2000 years old.',nextVersion:'Contrary to popular belief, Lorem Ipsum is not just a random text. It has roots in a piece of classical Latin literature from 75 BC, making it over 2000 years old.'};
 	}
 
-	plotDiff = function(one, other){
-		let diff = jsdiff.diffChars(one, other);
+	handleChange = function(e){
+		this.setState({nextVersion: e.target.value});
+	}
+
+	plotDiff = function(one2, other){
+		let diff = jsdiff.diffChars(one2, other);
 		let start = 0;
 		let end = 0;
 		let operations = [];
-		for(let i=0;i<diff.length;i++){
+		for (let i=0;i<diff.length;i++){
 			start = end;
-			if(diff[i].removed){
+			if (diff[i].removed){
 				operations.push({type:'delete',from:start,count:diff[i].count,text:diff[i].value})
 			}
 			else if (diff[i].added) {
 				end = start + diff[i].count;
 				operations.push({type:'add',from:start,count:diff[i].count,text:diff[i].value})
 			}
-			else{
+			else {
 				end = start + diff[i].count;
 			}
 		}
 		return(
 			<div>
 				{operations.map((operation)=>(<p>{operation.type} ({operation.from},{operation.count}): {operation.text} </p>))}
-
 			</div>)
 	}
 
 	render() {
-		let one = 'Contrary to belief, Lorem Ipsum is not just a random text. It has roots in a piece of classical Latin literature from 75 BC, making it over 2000 years old.';
-		let other = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.';
-		let i =0;
 		return (
 			<div class={style.home}>
 				<h1>Home</h1>
 				<p>This is the Home component.</p>
-				<p>{one}</p>
-				<p>{other}</p>
-				{this.plotDiff(one, other)}
+				<p>{this.state.firstVersion}</p>
+				<textarea value={this.state.nextVersion} onChange={this.handleChange} style="width: 100%;height: 3em;"></textarea>
+				{this.plotDiff(this.state.firstVersion, this.state.nextVersion)}
 			</div>
 		);
 	}
